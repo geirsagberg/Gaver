6310fdec76aa462fbd0d82d9a2e93f71d418e46e
Geir Sagberg <geir.sagberg@gmail.com>
2016-06-27 22:02:18 +0200

    Removed typescript

--- Gaver.Web/ClientApp/boot-server.jsx
+++ Gaver.Web/ClientApp/boot-server.jsx
@@ -1,32 +1,31 @@
-import * as React from 'react';
-import { Provider } from 'react-redux';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-import routes from './routes';
-import configureStore from './configureStore';
+import * as React from 'react'
+import { Provider } from 'react-redux'
+import { renderToString } from 'react-dom/server'
+import { match, RouterContext } from 'react-router'
+import routes from './routes'
+import configureStore from './configureStore'
 export default function (params) {
-    return new Promise((resolve, reject) => {
-        // Match the incoming request against the list of client-side routes
-        match({ routes, location: params.location }, (error, redirectLocation, renderProps) => {
-            if (error) {
-                throw error;
-            }
-            // Build an instance of the application
-            const store = configureStore();
-            const app = (<Provider store={store}>
-                    <RouterContext {...renderProps}/>
-                </Provider>);
-            // Perform an initial render that will cause any async tasks (e.g., data access) to begin
-            renderToString(app);
-            // Once the tasks are done, we can perform the final render
-            // We also send the redux store state, so the client can continue execution where the server left off
-            params.domainTasks.then(() => {
-                resolve({
-                    html: renderToString(app),
-                    globals: { initialReduxState: store.getState() }
-                });
-            }, reject); // Also propagate any errors back into the host application
-        });
-    });
+  return new Promise((resolve, reject) => {
+    // Match the incoming request against the list of client-side routes
+    match({ routes, location: params.location }, (error, redirectLocation, renderProps) => {
+      if (error) {
+        throw error
+      }
+      // Build an instance of the application
+      const store = configureStore()
+      const app = (<Provider store={store}>
+                     <RouterContext {...renderProps}/>
+                   </Provider>)
+      // Perform an initial render that will cause any async tasks (e.g., data access) to begin
+      renderToString(app)
+      // Once the tasks are done, we can perform the final render
+      // We also send the redux store state, so the client can continue execution where the server left off
+      params.domainTasks.then(() => {
+        resolve({
+          html: renderToString(app),
+          globals: { initialReduxState: store.getState() }
+        })
+      }, reject) // Also propagate any errors back into the host application
+    })
+  })
 }
-//# sourceMappingURL=boot-server.jsx.map
--- Gaver.Web/ClientApp/components/Layout.jsx
+++ Gaver.Web/ClientApp/components/Layout.jsx
@@ -1,17 +1,23 @@
-import * as React from 'react';
-import { NavMenu } from './NavMenu';
+import * as React from 'react'
+import { NavMenu } from './NavMenu'
+
 export class Layout extends React.Component {
-    render() {
-        return <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-sm-3'>
-                    <NavMenu />
-                </div>
-                <div className='col-sm-9'>
-                    {this.props.body}
-                </div>
-            </div>
-        </div>;
-    }
+  render () {
+    return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-3">
+          <NavMenu />
+        </div>
+        <div className="col-sm-9">
+          {this.props.children}
+        </div>
+      </div>
+    </div>
+    )
+  }
 }
-//# sourceMappingURL=Layout.jsx.map
+
+Layout.propTypes = {
+  children: React.PropTypes.element
+}
