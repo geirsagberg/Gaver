a32660809d19c09205e2c1f1313ecf6eb49472df
Geir Sagberg <geir.sagberg@gmail.com>
2016-10-25 19:50:48 +0200

    Fix styles

--- src/Gaver.Web/webpack.config.js
+++ src/Gaver.Web/webpack.config.js
@@ -6,6 +6,7 @@ var prodConfig = require('./webpack.config.prod')
 var isDevelopment = process.env.ASPNETCORE_ENVIRONMENT === 'Development'
 var autoPrefixer = require('autoprefixer')
 var precss = require('precss')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = merge({
   resolve: {
@@ -17,7 +18,7 @@ module.exports = merge({
       { test: /jquery\.js$/, loader: 'expose?jQuery!expose?$' },
       { test: /\.js(x?)$/, include: /ClientApp/, loader: 'babel' },
       { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url?limit=100000' },
-      { test: /\.css$/, loader: 'style!css!postcss?sourceMap=inline' }
+      { test: /\.css$/, loader: isDevelopment ? 'style!css!postcss?sourceMap=inline' : ExtractTextPlugin.extract('style', 'css!postcss?sourceMap=inline') }
     ]
   },
   postcss: function () {
@@ -36,5 +37,7 @@ module.exports = merge({
       'process.env.NODE_ENV': JSON.stringify(isDevelopment ? 'development' : 'production')
     }),
     new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
-  ]
+  ].concat(isDevelopment ? [] : [
+    new ExtractTextPlugin('styles.css')
+  ])
 }, isDevelopment ? devConfig : prodConfig)
