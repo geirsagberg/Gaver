7c193936c67a37586a2b7b7655393ae844a51cf4
Geir Sagberg <geir.sagberg@gmail.com>
2019-04-17 17:53:57 +0200

    Remove old code

--- src/Gaver.Web/ClientApp/store/data/users/index.ts
+++
@@ -1,8 +0,0 @@
-import produce from 'immer'
-import { UserModel } from '~/types/data'
-
-export type UserData = Dictionary<UserModel>
-
-const initialState: UserData = {}
-
-export const reducer = produce((draft: UserData, action) => {}, initialState)
